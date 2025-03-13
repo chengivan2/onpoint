44fe6e2f09f2795fb51d09dafa233bb206192527
chengivan <chengoleivan@gmail.com>
2025-03-13 19:44:57 +0300

    Modified header components styling

--- src/app/rootcomponents/header.tsx
+++ src/app/rootcomponents/header.tsx
@@ -5,7 +5,7 @@ import ThemeToggle from "./themetoggle";
 export default function Header() {
   return (
     <header className="absolute px-[1rem] flex flex-row w-full justify-between items-center bg-lightmode-header-bg-color dark:bg-darkmode-header-bg-color">
-      <h1 className="flex-grow text-[#F8F8F8] dark:text-[#181818]">Logo</h1>
+      <h1 className="flex-grow text-lightmode-text-color dark:text-[#181818]">Logo</h1>
       <Menu />
       <div
       className="flex flex-row flex-end gap-4">
--- src/app/rootcomponents/themetoggle.tsx
+++ src/app/rootcomponents/themetoggle.tsx
@@ -9,13 +9,12 @@ export default function ThemeToggle() {
 
   return (
     <Button
-      variant="outline"
       size="icon"
-      className="rounded-full hover:cursor-pointer"
+      className="rounded-full hover:cursor-pointer active:outline-0"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <FaSun className="absolute h-12 w-12 rotate-0 scale-100 dark:-rotate-90 dark:scale-0"></FaSun>
-      <FaMoon className="absolute h-12 w-12 rotate-90 scale-0 dark:-rotate-0 dark:scale-100"></FaMoon>
+      <FaSun className="absolute h-12 w-12 rotate-0 scale-100 text-darkmode-bg-color dark:-rotate-90 dark:scale-0"></FaSun>
+      <FaMoon className="absolute h-12 w-12 rotate-90 scale-0 dark:-rotate-22 dark:scale-100"></FaMoon>
     </Button>
   );
 }
