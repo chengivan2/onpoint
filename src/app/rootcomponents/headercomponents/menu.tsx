c3112a463a11134d436d6ed0f8e391fc4040797e
chengivan <chengoleivan@gmail.com>
2025-03-13 20:01:17 +0300

    Added more styling to the mobile header version

--- src/app/rootcomponents/headercomponents/menu.tsx
+++ src/app/rootcomponents/headercomponents/menu.tsx
@@ -1,17 +1,40 @@
+"use client";
+
 import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Menu() {
   return (
-    <div 
-    className="flex-grow px-[1rem]" 
-    >
-        <ul 
-        className="flex flex-col gap-[5rem] lg:flex-row lg:align-center">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/">Blog</Link></li>
-            <li><Link href="/">Contact</Link></li>
-            <li><Link href="/">About</Link></li>
-        </ul>
+    <div className="flex-grow px-[1rem]">
+      <button
+        className="lg:hidden hover:cursor-pointer"
+        onClick={() => {
+          const menu = document.getElementById("menu");
+          if (menu) {
+            menu.classList.toggle("hidden");
+          }
+        }}
+      >
+        <GiHamburgerMenu className="text-darkmode-bg-color dark:text-lightmode-bg-color" />
+      </button>
+
+      <ul
+        className="absolute bg-lightmode-bg-color border-2  border-darkmode-bg-color rounded hidden flex-col gap-[5rem] lg:flex lg:flex-row lg:align-center"
+        id="menu"
+      >
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/">Blog</Link>
+        </li>
+        <li>
+          <Link href="/">Contact</Link>
+        </li>
+        <li>
+          <Link href="/">About</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
