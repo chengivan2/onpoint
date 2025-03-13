5b38f084a949948cbe18cdabb9c313900c201e21
chengivan <chengoleivan@gmail.com>
2025-03-13 22:48:36 +0300

    Added styling to main

--- src/app/rootcomponents/homepagecomponents/main.tsx
+++ src/app/rootcomponents/homepagecomponents/main.tsx
@@ -1,8 +1,10 @@
 export default function Main() {
   return (
-    <main className="relative min-w-[100%] px-[1rem]">
+    <main className="relative min-w-[100%] min-h-[100vh] px-[1rem]">
     <h2
-    className="text-lightmode-text-color dark:text-darkmode-text-color">Travely</h2>
+    className="text-lightmode-text-color dark:text-darkmode-text-color">
+        Travely
+    </h2>
   </main>
   )
 }
