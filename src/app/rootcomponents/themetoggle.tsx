7de05e14f797e1c16ea9d4e1980e28a69c551143
chengivan <chengoleivan@gmail.com>
2025-03-14 13:13:54 +0300

    Styled the theme toggler

--- src/app/rootcomponents/themetoggle.tsx
+++ src/app/rootcomponents/themetoggle.tsx
@@ -13,8 +13,8 @@ export default function ThemeToggle() {
       className="rounded-full hover:cursor-pointer active:outline-0"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <FaSun className="absolute h-12 w-12 rotate-0 scale-100 text-darkmode-bg-color dark:-rotate-90 dark:scale-0"></FaSun>
-      <FaMoon className="absolute h-12 w-12 rotate-90 scale-0 dark:-rotate-22 dark:scale-100"></FaMoon>
+      <FaMoon className="absolute h-12 w-12 -rotate-21 duration-500 scale-100 text-darkmode-bg-color dark:-rotate-0 dark:scale-0"></FaMoon>
+      <FaSun className="absolute h-12 w-12 rotate-90 scale-0 dark:-rotate-22 duration-500 dark:scale-100"></FaSun>
     </Button>
   );
 }
