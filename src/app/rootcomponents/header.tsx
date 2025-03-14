08f7e8f18cee04323ed52794818439d20a653472
chengivan <chengoleivan@gmail.com>
2025-03-14 13:25:14 +0300

    Displayed header logo component in the header

--- src/app/rootcomponents/header.tsx
+++ src/app/rootcomponents/header.tsx
@@ -1,11 +1,12 @@
 import HeaderCtas from "./headercomponents/ctas";
+import HeaderLogo from "./headercomponents/logo";
 import Menu from "./headercomponents/menu";
 import ThemeToggle from "./themetoggle";
 
 export default function Header() {
   return (
     <header className="relative px-[1rem] flex flex-row w-full justify-between items-center bg-lightmode-header-bg-color dark:bg-darkmode-header-bg-color">
-      <h1 className="flex-grow text-lightmode-text-color dark:text-darkmode-text-color">Logo</h1>
+      <HeaderLogo />
       <Menu />
       <div
       className="flex flex-row flex-end gap-4">
