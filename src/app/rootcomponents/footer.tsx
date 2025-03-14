90d3fe402c3d862b078a8dd5620333bcd624947b
chengivan <chengoleivan@gmail.com>
2025-03-14 20:48:47 +0300

    Added footer links array

--- src/app/rootcomponents/footer.tsx
+++ src/app/rootcomponents/footer.tsx
@@ -1,8 +1,113 @@
-
 export default function Footer() {
-  return (
-    <footer className="border-b bg-lightmode-footer-bg-color pt-20 dark:bg-darkmode-footer-bg-color">
 
-    </footer>
-  )
+  const links = [
+    {
+      group: "Top Destinations",
+      items: [
+        {
+          title: "Diani",
+          href: "#",
+        },
+        {
+          title: "Nairobi",
+          href: "#",
+        },
+        {
+          title: "Arusha",
+          href: "#",
+        },
+        {
+          title: "Masai Mara",
+          href: "#",
+        },
+        {
+          title: "Mombasa",
+          href: "#",
+        },
+        {
+          title: "Doha",
+          href: "#",
+        },
+      ],
+    },
+    // {
+    //   group: "Solution",
+    //   items: [
+    //     {
+    //       title: "Startup",
+    //       href: "#",
+    //     },
+    //     {
+    //       title: "Freelancers",
+    //       href: "#",
+    //     },
+    //     {
+    //       title: "Organizations",
+    //       href: "#",
+    //     },
+    //     {
+    //       title: "Students",
+    //       href: "#",
+    //     },
+    //     {
+    //       title: "Collaboration",
+    //       href: "#",
+    //     },
+    //     {
+    //       title: "Design",
+    //       href: "#",
+    //     },
+    //     {
+    //       title: "Management",
+    //       href: "#",
+    //     },
+    //   ],
+    // },
+    {
+      group: "Company",
+      items: [
+        {
+          title: "About",
+          href: "#",
+        },
+        {
+          title: "Careers",
+          href: "#",
+        },
+        {
+          title: "Blog",
+          href: "#",
+        },
+        {
+          title: "Contact",
+          href: "#",
+        },
+        {
+          title: "Help",
+          href: "#",
+        },
+      ],
+    },
+    {
+      group: "Legal",
+      items: [
+        {
+          title: "Licence",
+          href: "#",
+        },
+        {
+          title: "Privacy",
+          href: "#",
+        },
+        {
+          title: "Terms & Conditions",
+          href: "#",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <footer className="border-b bg-lightmode-footer-bg-color pt-20 dark:bg-darkmode-footer-bg-color"></footer>
+  );
 }
