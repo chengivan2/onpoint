8af9c3878fe0478a81371a1daeae9ea53d9b06a0
chengivan <chengoleivan@gmail.com>
2025-03-16 11:01:24 +0300

    Used the new header text variations

--- src/app/rootcomponents/headercomponents/menu.tsx
+++ src/app/rootcomponents/headercomponents/menu.tsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 
 export default function Menu() {
   return (
-    <div className="text-[0.8rem] lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
-      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
+    <div className="text-[0.8rem] text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
+      <Link href="#" className="block">
         Home
       </Link>
-      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
+      <Link href="#" className="block">
         About
       </Link>
-      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
+      <Link href="#" className="block">
         Services
       </Link>
-      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
-        Contact
+      <Link href="#" className="block">
+        Contac
       </Link>
     </div>
   );
