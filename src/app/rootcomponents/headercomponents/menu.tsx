c16e718a6e640faf7d25bb6dbb632b1aba2b3614
chengivan <chengoleivan@gmail.com>
2025-03-16 10:01:49 +0300

    Switched to the Link tag in the menu component

--- src/app/rootcomponents/headercomponents/menu.tsx
+++ src/app/rootcomponents/headercomponents/menu.tsx
@@ -6,18 +6,18 @@ import { GiHamburgerMenu } from "react-icons/gi";
 export default function Menu() {
   return (
     <div className="lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
-      <a href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
+      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
         Home
-      </a>
-      <a href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
+      </Link>
+      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
         About
-      </a>
-      <a href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
+      </Link>
+      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
         Services
-      </a>
-      <a href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
+      </Link>
+      <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
         Contact
-      </a>
+      </Link>
     </div>
   );
 }
