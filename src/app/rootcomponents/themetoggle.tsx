f6f60958422593e5796d792afbd1bd4b0385e89c
chengivan <chengoleivan@gmail.com>
2025-03-16 16:09:49 +0300

    Replaced TbSunFilled with IoSunny in the theme toggler

--- src/app/rootcomponents/themetoggle.tsx
+++ src/app/rootcomponents/themetoggle.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import { FaMoon } from "react-icons/fa6";
-import { TbSunFilled } from "react-icons/tb";
+import { IoSunny } from "react-icons/io5";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -15,7 +15,7 @@ export default function ThemeToggle() {
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
       <FaMoon className="absolute h-12 w-12 -rotate-21 duration-300 scale-100 text-darkmode-bg-color dark:-rotate-0 dark:scale-0" />
-      <TbSunFilled className="absolute h-12 w-12 rotate-90 scale-0 dark:-rotate-22 duration-300 dark:scale-100" />
+      <IoSunny className="absolute h-12 w-12 rotate-90 scale-0 dark:-rotate-22 duration-300 dark:scale-100" />
     </Button>
   );
 }
