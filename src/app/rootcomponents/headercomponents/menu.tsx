2f8863684f41936b4d3f2634d88685070ab018ba
chengivan <chengoleivan@gmail.com>
2025-03-16 10:03:11 +0300

    Deleted the GiHamburger import from the menu file

--- src/app/rootcomponents/headercomponents/menu.tsx
+++ src/app/rootcomponents/headercomponents/menu.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import Link from "next/link";
-import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Menu() {
   return (
-    <div className="lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
+    <div className="text-[0.8rem] lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
       <Link href="#" className="block text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white">
         Home
       </Link>
