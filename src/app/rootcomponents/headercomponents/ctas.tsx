ef8f93ba238f31429ec2bbcc2664ffe2611a5584
chengivan <chengoleivan@gmail.com>
2025-03-16 22:33:51 +0300

    Made the first header cta's border radius rounded-full

--- src/app/rootcomponents/headercomponents/ctas.tsx
+++ src/app/rootcomponents/headercomponents/ctas.tsx
@@ -5,7 +5,7 @@ export default function HeaderCtas() {
     <div
     className="flex-grow" 
     >
-        <Button className="text-[0.8rem] hover:cursor-pointer bg-lightmode-btn-bg-color dark:bg-darkmode-btn-bg-color hover:bg-lightmode-btn-bg-hover-color dark:hover:bg-darkmode-btn-bg-hover-color text-lightmode-btn-text-color dark:text-darkmode-btn-text-color">
+        <Button className="text-[0.8rem] rounded-full hover:cursor-pointer bg-lightmode-btn-bg-color dark:bg-darkmode-btn-bg-color hover:bg-lightmode-btn-bg-hover-color dark:hover:bg-darkmode-btn-bg-hover-color text-lightmode-btn-text-color dark:text-darkmode-btn-text-color">
             Make Your Own Trip
         </Button>
     </div>
