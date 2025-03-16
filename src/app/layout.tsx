42e79bfcdd08f2c732b81a659ea65e39a991098a
chengivan <chengoleivan@gmail.com>
2025-03-16 21:04:12 +0300

    Now using Inter as the only font in the website 😎

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import ScrollToTop from './rootcomponents/ScrollToTop';
+import { inter } from './rootcomponents/fonts';
 
 export const metadata: Metadata = {
   title: "Travely",
@@ -14,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className="flex flex-col bg-lightmode-bg-color dark:bg-darkmode-bg-color">
+    <html lang="en" suppressHydrationWarning className={`${inter.variable}`}>
+      <body className={`font-sans antialiased flex flex-col bg-lightmode-bg-color dark:bg-darkmode-bg-color`}>
         <ThemeProvider
         attribute="class"
         enableSystem
