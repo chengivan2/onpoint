0cc105eff6f22cbac2614ad1de88b2efee98312f
chengivan <chengoleivan@gmail.com>
2025-03-16 11:04:23 +0300

    Moved the new header text variations inside the links' classnames

--- src/app/rootcomponents/headercomponents/menu.tsx
+++ src/app/rootcomponents/headercomponents/menu.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 
 export default function Menu() {
   return (
-    <div className="text-[0.8rem] text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
-      <Link href="#" className="block">
+    <div className="text-[0.8rem] lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
+      <Link href="#" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
         Home
       </Link>
-      <Link href="#" className="block">
+      <Link href="#" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
         About
       </Link>
-      <Link href="#" className="block">
+      <Link href="#" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
         Services
       </Link>
-      <Link href="#" className="block">
+      <Link href="#" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
         Contac
       </Link>
     </div>
