51afde43be542f4c0910c1de049ca881c6edabae
chengivan <chengoleivan@gmail.com>
2025-03-17 21:02:18 +0300

    Added hero test file's test image

--- src/app/rootcomponents/homepagecomponents/herosection.tsx
+++ src/app/rootcomponents/homepagecomponents/herosection.tsx
@@ -1,9 +1,12 @@
 
 export default function HerosectionOne() {
   return (
-    <section className='min-w-full min-h-[100vh]'>
+    <section className='relative px-[1.5rem] py-[2rem] min-w-full min-h-[100vh]'>
 
-        <div></div>
+        <div
+        className="min-w-full min-h-full rounded-[0.8rem]">
+            
+        </div>
         
     </section>
   )
