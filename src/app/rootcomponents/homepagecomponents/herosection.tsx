3d45c8b6b642709bc71e1bc95e9a2bc54d1433fa
chengivan <chengoleivan@gmail.com>
2025-03-18 14:54:10 +0300

    Introduced the dark and ligh mode hero section bgs

--- src/app/rootcomponents/homepagecomponents/herosection.tsx
+++ src/app/rootcomponents/homepagecomponents/herosection.tsx
@@ -4,7 +4,7 @@ export default function HerosectionOne() {
   return (
     <section className="relative px-[0.1rem] lg:px-[1rem] mt-20 py-[0.8rem] lg:py-[2rem] min-w-full min-h-[100vh]">
 
-      <div className="relative px-[0.1rem] py-[0.1rem] min-w-full h-[98vh] rounded-[0.8rem] bg-cover bg-center bg-[url(/images/herotestimage.jpg)]">
+      <div className="relative px-[0.1rem] py-[0.1rem] min-w-full h-[98vh] rounded-[0.8rem] bg-cover bg-center bg-[url(/images/onpointherolightmoodebg.png) dark:bg-[url(/images/onpointherodarkmoodebg.png)]">
 
         <div className="absolute inset-0 bg-[#1A1A1A] opacity-50 rounded-[0.8rem]"></div>
 
