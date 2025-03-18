a0e645ade53102c3bd46f21a4335c2c0c8e5c8d8
chengivan <chengoleivan@gmail.com>
2025-03-18 21:55:55 +0300

    Installed @supabase/supabase-js and @supabase/ssr

--- src/app/rootcomponents/homepagecomponents/herosection.tsx
+++ src/app/rootcomponents/homepagecomponents/herosection.tsx
@@ -3,19 +3,16 @@ import AnimatedHeading from "@/app/rootcomponents/AnimatedHeading";
 export default function HerosectionOne() {
   return (
     <section className="relative px-[0.1rem] lg:px-[1rem] mt-20 py-[0.8rem] lg:py-[2rem] min-w-full min-h-[100vh]">
-
       <div className="relative px-[0.1rem] py-[0.1rem] min-w-full h-[98vh] rounded-[0.8rem] bg-cover bg-center bg-[url(/images/herobgimage.jpg)]">
-
         <div className="absolute inset-0 bg-[#1A1A1A] opacity-40 dark:opacity-50 rounded-[0.8rem]"></div>
 
         <div className="relative min-h-full min-w-full flex flex-col justify-center items-center">
-            <AnimatedHeading
-            words={['Toward', 'Explore', 'Discover']}
-            restOfHeading="More Travel Time" />
+          <AnimatedHeading
+            words={["Toward", "Explore", "Discover"]}
+            restOfHeading="More Travel Time"
+          />
         </div>
-
       </div>
-
     </section>
   );
 }
