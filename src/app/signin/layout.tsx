098b8a32890684a2a96c4640ae4846ab478e24e5
chengivan <chengoleivan@gmail.com>
2025-03-19 14:02:40 +0300

    Modified the signin page

--- src/app/signin/layout.tsx
+++ src/app/signin/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { ThemeProvider } from "next-themes";
 import ScrollToTop from "../rootcomponents/ScrollToTop";
 import { inter } from '../rootcomponents/fonts';
