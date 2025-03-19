3122bad2dadaad9352f6bd08c8d70d4546d8c4ac
chengivan <chengoleivan@gmail.com>
2025-03-19 20:13:22 +0300

    Modified the hero section

--- src/app/rootcomponents/homepagecomponents/herosection.tsx
+++ src/app/rootcomponents/homepagecomponents/herosection.tsx
@@ -2,7 +2,7 @@ import AnimatedHeading from "@/app/rootcomponents/AnimatedHeading";
 
 export default function HerosectionOne() {
   return (
-    <section className="relative px-[0.1rem] lg:px-[1rem] mt-20 py-[0.8rem] lg:py-[2rem] min-w-full min-h-[100vh]">
+    <section className="relative bg-transparent px-[0.1rem] lg:px-[1rem] mt-20 py-[0.8rem] lg:py-[2rem] min-w-full min-h-[100vh]">
       <div className="relative px-[0.1rem] py-[0.1rem] min-w-full h-[98vh] rounded-[0.8rem] bg-cover bg-center bg-[url(/images/herobgimage.jpg)]">
         <div className="absolute inset-0 bg-darkmode-bg-color opacity-30 dark:opacity-40 rounded-[0.8rem]"></div>
 
