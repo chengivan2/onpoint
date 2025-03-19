4b9f8667d3261613fbc3ebf0b3d08563732cfb89
chengivan <chengoleivan@gmail.com>
2025-03-19 12:32:47 +0300

    Added a layout file to the sign in page

---
+++ src/app/signin/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import ScrollToTop from "../rootcomponents/ScrollToTop";
+import { inter } from '../rootcomponents/fonts';
+
+export const metadata: Metadata = {
+  title: "Sign In - Travely",
+  description: "Sign in to manage your Travely account",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning className={`${inter.variable}`}>
+      <body className={`font-sans antialiased flex flex-col bg-lightmode-bg-color dark:bg-darkmode-bg-color`}>
+        <ThemeProvider
+        attribute="class"
+        enableSystem
+        defaultTheme="system">
+          {children}
+        </ThemeProvider>
+        <ScrollToTop />
+      </body>
+    </html>
+  );
+}
