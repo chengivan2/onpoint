4bb1ffa4ab4ab3f66d02792795efdda9d34dacb1
chengivan <chengoleivan@gmail.com>
2025-03-20 20:16:47 +0300

    Modified the styling of the hero section

--- src/app/rootcomponents/homepagecomponents/herosection.tsx
+++ src/app/rootcomponents/homepagecomponents/herosection.tsx
@@ -3,8 +3,8 @@ import AnimatedHeading from "@/app/rootcomponents/AnimatedHeading";
 export default function HerosectionOne() {
   return (
     <section className="relative bg-transparent px-[0.1rem] lg:px-[1rem] mt-20 py-[0.8rem] lg:py-[2rem] min-w-full min-h-[100vh]">
-      <div className="relative px-[0.1rem] py-[0.1rem] min-w-full h-[98vh] rounded-[0.8rem] bg-cover bg-center bg-[url(/images/herobgimage.jpg)]">
-        <div className="absolute inset-0 bg-darkmode-bg-color opacity-30 dark:opacity-40 rounded-[0.8rem]"></div>
+      <div className="relative px-[0.1rem] py-[0.1rem] min-w-full h-[98vh] rounded-[0.7rem] bg-cover bg-center bg-[url(/images/herobgimage.jpg)]">
+        <div className="absolute inset-0 bg-darkmode-bg-color opacity-30 dark:opacity-40 rounded-[0.7rem]"></div>
 
         <div className="relative min-h-full min-w-full flex flex-col justify-center items-center">
           <AnimatedHeading
@@ -16,7 +16,7 @@ export default function HerosectionOne() {
         <div className="relative min-h-full min-w-full flex flex-col justify-center items-center">
           <h4>Work Hard Play Hard</h4>
         </div>
-        
+
       </div>
     </section>
   );
