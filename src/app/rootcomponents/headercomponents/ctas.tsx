ed4b91da7a5bfbdc33ca8b8a66e3e60ed58dd29a
chengivan <chengoleivan@gmail.com>
2025-03-22 23:30:39 +0300

    Styled the parent of the header cta buttons

--- src/app/rootcomponents/headercomponents/ctas.tsx
+++ src/app/rootcomponents/headercomponents/ctas.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import AuthButton from "../authbutton";
 
 export default function HeaderCtas() {
   return (
     <div
-    className="flex-grow" 
+    className="flex flex-grow flex-col lg:flex-row gap-[1rem]" 
     >
+        <AuthButton />
         <Button className="text-[0.8rem] rounded-full duration-300 hover:cursor-pointer bg-lightmode-btn-bg-color dark:bg-darkmode-btn-bg-color hover:bg-lightmode-btn-bg-hover-color dark:hover:bg-darkmode-btn-bg-hover-color text-lightmode-btn-text-color dark:text-darkmode-btn-text-color">
             Make Your Own Trip
         </Button>
