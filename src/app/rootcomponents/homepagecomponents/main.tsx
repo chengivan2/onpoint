93940e5e326de3f7c49130e1e18162a3fe1ffb0b
chengivan <chengoleivan@gmail.com>
2025-03-23 17:07:51 +0300

    Styled the home's main component

--- src/app/rootcomponents/homepagecomponents/main.tsx
+++ src/app/rootcomponents/homepagecomponents/main.tsx
@@ -3,7 +3,7 @@ import HerosectionOne from "./herosection";
 
 export default function Main() {
   return (
-    <main className="relative min-w-[100%] min-h-[100vh] px-[1rem]">
+    <main className="relative min-w-[100%] min-h-[100vh]">
       <Header />
       <HerosectionOne />
   </main>
