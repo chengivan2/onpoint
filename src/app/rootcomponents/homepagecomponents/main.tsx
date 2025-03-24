d9035ccc054067e389e094c2ffc3a31b6cefe9fd
chengivan <chengoleivan@gmail.com>
2025-03-24 21:30:08 +0300

    Displaying HomeTopDestinations on the home main

--- src/app/rootcomponents/homepagecomponents/main.tsx
+++ src/app/rootcomponents/homepagecomponents/main.tsx
@@ -1,11 +1,13 @@
 import Header from "../header";
 import HerosectionOne from "./herosection";
+import HomeTopDestinations from "./hometopdestinations";
 
 export default function Main() {
   return (
     <main className="relative min-w-[100%] min-h-[100vh]">
       <Header />
       <HerosectionOne />
+      <HomeTopDestinations />
   </main>
   )
 }
