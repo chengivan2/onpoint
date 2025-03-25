1355a401e862200d1ca69f2a5b3e38e2ad06f1aa
chengivan <chengoleivan@gmail.com>
2025-03-25 14:04:26 +0300

    Top destination component is now wrapped in section tags

--- src/app/rootcomponents/homepagecomponents/hometopdestinations.tsx
+++ src/app/rootcomponents/homepagecomponents/hometopdestinations.tsx
@@ -1,9 +1,9 @@
 export default function HomeTopDestinations() {
   return (
-    <div className="relative flex flex-col min-w-full">
+    <section className="relative flex flex-col min-w-full">
       <div className="flex relative min-w-full justify-center">
       <h2>Top Destinations</h2>
       </div>
-    </div>
+    </section>
   )
 }
