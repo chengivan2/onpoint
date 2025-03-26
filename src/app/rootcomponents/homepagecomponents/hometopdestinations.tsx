15a8b685806aee5f8a619621fb3582c0617701e6
chengivan <chengoleivan@gmail.com>
2025-03-26 15:19:54 +0300

    Removed the policies, from the destinations table, that were recursively referencing the users table

--- src/app/rootcomponents/homepagecomponents/hometopdestinations.tsx
+++ src/app/rootcomponents/homepagecomponents/hometopdestinations.tsx
@@ -1,9 +1,14 @@
+import HomeDestinationCard from "./destinationcard";
+
 export default function HomeTopDestinations() {
   return (
     <section className="relative flex flex-col min-w-full">
       <div className="flex relative min-w-full justify-center">
         <h2 className="text-4xl font-black text-lightmode-heading-color dark:text-darkmode-heading-color">Top Destinations</h2>
       </div>
+
+      <HomeDestinationCard />
+
     </section>
   );
 }
