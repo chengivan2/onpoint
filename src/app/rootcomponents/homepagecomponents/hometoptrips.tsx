efd7c7d14a1a956c0f3d4952d516147b9eef96ff
chengivan <chengoleivan@gmail.com>
2025-03-28 23:39:24 +0300

    Modified styling in the top trips section of the home page

--- src/app/rootcomponents/homepagecomponents/hometoptrips.tsx
+++ src/app/rootcomponents/homepagecomponents/hometoptrips.tsx
@@ -1,9 +1,8 @@
-import HomeDestinationCard from "./destinationcard";
 import HomeTripCards from "./hometrips";
 
 export default function HomeTopTrips() {
   return (
-    <section className="relative flex flex-col min-w-full">
+    <section className="py-[3rem] bg-green-50/20 dark:bg-green-900/10 relative flex flex-col min-w-full">
       <div className="flex relative min-w-full justify-center">
         <h2 className="text-4xl font-black text-lightmode-heading-color dark:text-darkmode-heading-color">Top Trips</h2>
       </div>
